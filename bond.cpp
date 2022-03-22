eb4ccb9fbfdf2e33dbd463e5ddcc783a9a1349ff
Alex Wade <38112687+adw62@users.noreply.github.com>
2022-03-22 13:21:49 +0000

    Add files via upload

--- bond.cpp
+++ bond.cpp
@@ -1,9 +1,10 @@
 #include <CL/sycl.hpp>
 #include <dpct/dpct.hpp>
 #include <iostream>
+#include <cassert>
+#include <cmath>
 
 #define KERNEL extern "C"
-#define PADDED_NUM_ATOMS 32
 
 inline sycl::float3 make_float3(float a) {
     return sycl::float3(a, a, a);
@@ -47,7 +48,6 @@ inline void operator+=(sycl::float3 &a, sycl::float3 b) {
     a.x() += b.x(); a.y() += b.y(); a.z() += b.z();
 }
 } // namespace dpct_operator_overloading
-
 /*
 DPCT1011:3: The tool detected overloaded operators for built-in vector types,
 which may conflict with the SYCL 1.2.1 standard operators (see 4.10.2.1 Vec
@@ -69,13 +69,12 @@ extern "C" void computeBondedForces(
     unsigned long long *__restrict__ forceBuffer,
     float *__restrict__ energyBuffer, const sycl::float4 *__restrict__ posq,
     const sycl::uint2 *__restrict__ atomIndices0_0, sycl::float2 *params,
-    int num_atoms, sycl::nd_item<3> item_ct1, const sycl::stream &stream_ct1) {
-
+    int num_atoms, sycl::nd_item<3> item_ct1) {
 float energy = 0;
 for (unsigned int index =
          item_ct1.get_group(2) * item_ct1.get_local_range().get(2) +
          item_ct1.get_local_id(2);
-     index < num_atoms;
+     index < num_atoms - 1;
      index += item_ct1.get_local_range().get(2) * item_ct1.get_group_range(2)) {
     sycl::uint2 atoms0 = atomIndices0_0[index];
     unsigned int atom1 = atoms0.x();
@@ -93,14 +92,13 @@ sycl::float2 bondParams = params[index];
 float deltaIdeal = r - bondParams.x();
 energy += 0.5f * bondParams.y() * deltaIdeal * deltaIdeal;
 float dEdR = bondParams.y() * deltaIdeal;
-
 dEdR = (r > 0) ? (dEdR / r) : 0;
 dpct_operator_overloading::operator*=(delta, dEdR);
 sycl::float3 force1 = delta;
 sycl::float3 force2 = dpct_operator_overloading::operator-(delta);
 
     /*
-    DPCT1039:6: The generated code assumes that "&forceBuffer[atom1]" points to
+    DPCT1039:4: The generated code assumes that "&forceBuffer[atom1]" points to
     the global memory address space. If it points to a local memory address
     space, replace "sycl::global_ptr" with "sycl::local_ptr".
     */
@@ -109,29 +107,26 @@ sycl::float3 force2 = dpct_operator_overloading::operator-(delta);
         .fetch_add(
             static_cast<unsigned long long>(realToFixedPoint(force1.x())));
     /*
-    DPCT1039:7: The generated code assumes that
-    "&forceBuffer[atom1+PADDED_NUM_ATOMS]" points to the global memory address
-    space. If it points to a local memory address space, replace
-    "sycl::global_ptr" with "sycl::local_ptr".
+    DPCT1039:5: The generated code assumes that "&forceBuffer[atom1+num_atoms]"
+    points to the global memory address space. If it points to a local memory
+    address space, replace "sycl::global_ptr" with "sycl::local_ptr".
     */
     sycl::atomic<unsigned long long>(
-        sycl::global_ptr<unsigned long long>(
-            &forceBuffer[atom1 + PADDED_NUM_ATOMS]))
+        sycl::global_ptr<unsigned long long>(&forceBuffer[atom1 + num_atoms]))
         .fetch_add(
             static_cast<unsigned long long>(realToFixedPoint(force1.y())));
     /*
-    DPCT1039:8: The generated code assumes that
-    "&forceBuffer[atom1+PADDED_NUM_ATOMS*2]" points to the global memory address
-    space. If it points to a local memory address space, replace
-    "sycl::global_ptr" with "sycl::local_ptr".
+    DPCT1039:6: The generated code assumes that
+    "&forceBuffer[atom1+num_atoms*2]" points to the global memory address space.
+    If it points to a local memory address space, replace "sycl::global_ptr"
+    with "sycl::local_ptr".
     */
-    sycl::atomic<unsigned long long>(
-        sycl::global_ptr<unsigned long long>(
-            &forceBuffer[atom1 + PADDED_NUM_ATOMS * 2]))
+    sycl::atomic<unsigned long long>(sycl::global_ptr<unsigned long long>(
+                                         &forceBuffer[atom1 + num_atoms * 2]))
         .fetch_add(
             static_cast<unsigned long long>(realToFixedPoint(force1.z())));
     /*
-    DPCT1078:9: Consider replacing memory_order::acq_rel with
+    DPCT1078:7: Consider replacing memory_order::acq_rel with
     memory_order::seq_cst for correctness if strong memory order restrictions
     are needed.
     */
@@ -139,7 +134,7 @@ sycl::float3 force2 = dpct_operator_overloading::operator-(delta);
         sycl::ext::oneapi::memory_order::acq_rel,
         sycl::ext::oneapi::memory_scope::work_group);
     /*
-    DPCT1039:10: The generated code assumes that "&forceBuffer[atom2]" points to
+        DPCT1039:8: The generated code assumes that "&forceBuffer[atom2]" points to
     the global memory address space. If it points to a local memory address
     space, replace "sycl::global_ptr" with "sycl::local_ptr".
     */
@@ -148,29 +143,26 @@ sycl::float3 force2 = dpct_operator_overloading::operator-(delta);
         .fetch_add(
             static_cast<unsigned long long>(realToFixedPoint(force2.x())));
     /*
-    DPCT1039:11: The generated code assumes that
-    "&forceBuffer[atom2+PADDED_NUM_ATOMS]" points to the global memory address
-    space. If it points to a local memory address space, replace
-    "sycl::global_ptr" with "sycl::local_ptr".
+    DPCT1039:9: The generated code assumes that "&forceBuffer[atom2+num_atoms]"
+    points to the global memory address space. If it points to a local memory
+    address space, replace "sycl::global_ptr" with "sycl::local_ptr".
     */
     sycl::atomic<unsigned long long>(
-        sycl::global_ptr<unsigned long long>(
-            &forceBuffer[atom2 + PADDED_NUM_ATOMS]))
+        sycl::global_ptr<unsigned long long>(&forceBuffer[atom2 + num_atoms]))
         .fetch_add(
             static_cast<unsigned long long>(realToFixedPoint(force2.y())));
     /*
-    DPCT1039:12: The generated code assumes that
-    "&forceBuffer[atom2+PADDED_NUM_ATOMS*2]" points to the global memory address
-    space. If it points to a local memory address space, replace
-    "sycl::global_ptr" with "sycl::local_ptr".
+    DPCT1039:10: The generated code assumes that
+    "&forceBuffer[atom2+num_atoms*2]" points to the global memory address space.
+    If it points to a local memory address space, replace "sycl::global_ptr"
+    with "sycl::local_ptr".
     */
-    sycl::atomic<unsigned long long>(
-        sycl::global_ptr<unsigned long long>(
-            &forceBuffer[atom2 + PADDED_NUM_ATOMS * 2]))
+    sycl::atomic<unsigned long long>(sycl::global_ptr<unsigned long long>(
+                                         &forceBuffer[atom2 + num_atoms * 2]))
         .fetch_add(
             static_cast<unsigned long long>(realToFixedPoint(force2.z())));
     /*
-    DPCT1078:13: Consider replacing memory_order::acq_rel with
+    DPCT1078:11: Consider replacing memory_order::acq_rel with
     memory_order::seq_cst for correctness if strong memory order restrictions
     are needed.
     */
@@ -180,6 +172,7 @@ sycl::float3 force2 = dpct_operator_overloading::operator-(delta);
 }
 energyBuffer[item_ct1.get_group(2) * item_ct1.get_local_range().get(2) +
              item_ct1.get_local_id(2)] += energy;
+//printf("%6.4lf\n", energy);
 }
 
 int main(int argc, char **argv) try {
@@ -190,78 +183,89 @@ if (argc != 2) {
     printf("Usage: %s <NINT>\n", argv[0]);
     exit(1);
 }
-int num_atoms = atoi(argv[1]);
-//int num_atoms = 10000;
+int num_atoms = (atoi(argv[1])*200)+1;
+int num_bonds = num_atoms-1;
 
-//make idxs
-sycl::uint2 *atomIndices0_0 = new sycl::uint2[num_atoms]();
-sycl::uint2 *atomIndices0_0_h = new sycl::uint2[num_atoms]();
-for (unsigned int i = 0; i < num_atoms; i++) {
-  atomIndices0_0_h[i] = {0, i};
+//x, y, z for atoms which have a spring connected to 0,0,0
+float p = 5.0;
+//spring constant for all springs
+float k = 2;
+//equilibrium separation for all springs
+float r0 = 1;
+
+//make bond idxs (these ids denote what atoms have springs between them)
+sycl::uint2 *atomIndices0_0 = new sycl::uint2[num_bonds]();
+sycl::uint2 *atomIndices0_0_h = new sycl::uint2[num_bonds]();
+for (unsigned int i = 0; i < num_bonds; i++) {
+  atomIndices0_0_h[i] = {0, i+1};
 }
 
-atomIndices0_0 = sycl::malloc_device<sycl::uint2>(num_atoms, q_ct1);
-q_ct1.memcpy(atomIndices0_0, atomIndices0_0_h, num_atoms * sizeof(sycl::uint2))
+atomIndices0_0 = sycl::malloc_device<sycl::uint2>(num_bonds, q_ct1);
+q_ct1.memcpy(atomIndices0_0, atomIndices0_0_h, num_bonds * sizeof(sycl::uint2))
     .wait();
 
-//make force running total
-unsigned long long* forceBuffer = new unsigned long long[num_atoms]();
-unsigned long long* forceBuffer_h = new unsigned long long[num_atoms]();
-for (int i = 0; i < num_atoms; i++) {
-  forceBuffer_h[i] = 0.0;
+//make bond params, (r0, k)
+sycl::float2 *params = new sycl::float2[num_bonds]();
+sycl::float2 *params_h = new sycl::float2[num_bonds]();
+for (int i = 0; i < num_bonds; i++) {
+  params_h[i] = {r0, k};
 }
 
-forceBuffer = sycl::malloc_device<unsigned long long>(num_atoms, q_ct1);
-q_ct1.memcpy(forceBuffer, forceBuffer_h, num_atoms * sizeof(unsigned long long))
-    .wait();
+params = sycl::malloc_device<sycl::float2>(num_bonds, q_ct1);
+q_ct1.memcpy(params, params_h, num_bonds * sizeof(sycl::float2)).wait();
 
 //make energy running total
-float* energyBuffer = new float[num_atoms]();
-float* energyBuffer_h = new float[num_atoms]();
-for (int i = 0; i < num_atoms; i++) {
+float* energyBuffer = new float[num_bonds]();
+float* energyBuffer_h = new float[num_bonds]();
+for (int i = 0; i < num_bonds; i++) {
   energyBuffer_h[i] = 0.0;
 }
 
-energyBuffer = sycl::malloc_device<float>(num_atoms, q_ct1);
-q_ct1.memcpy(energyBuffer, energyBuffer_h, num_atoms * sizeof(float)).wait();
+energyBuffer = sycl::malloc_device<float>(num_bonds, q_ct1);
+q_ct1.memcpy(energyBuffer, energyBuffer_h, num_bonds * sizeof(float)).wait();
 
-//make positions
+//make force running total
+unsigned long long* forceBuffer = new unsigned long long[num_atoms*3]();
+unsigned long long* forceBuffer_h = new unsigned long long[num_atoms*3]();
+for (int i = 0; i < num_atoms*3; i++) {
+  forceBuffer_h[i] = 0.0;
+}
+forceBuffer = sycl::malloc_device<unsigned long long>(3 * num_atoms, q_ct1);
+q_ct1
+    .memcpy(forceBuffer, forceBuffer_h,
+            3 * num_atoms * sizeof(unsigned long long))
+    .wait();
+
+//make positions (Place one atom at 0,0,0 and the rest at some user defined x,y,z)
 sycl::float4 *posq = new sycl::float4[num_atoms]();
 sycl::float4 *posq_h = new sycl::float4[num_atoms]();
 posq_h[0] = {0.0,0.0,0.0,0.0};
 for (int i = 1; i < num_atoms; i++) {
-  posq_h[i] = {5.0,5.0,5.0,0.0};
+  posq_h[i] = {p,p,p,0.0};
 }
 
 posq = sycl::malloc_device<sycl::float4>(num_atoms, q_ct1);
 q_ct1.memcpy(posq, posq_h, num_atoms * sizeof(sycl::float4)).wait();
 
-//make bond params, (r0, k)
-sycl::float2 *params = new sycl::float2[num_atoms]();
-sycl::float2 *params_h = new sycl::float2[num_atoms]();
-params_h[0] = {1, 0};
-for (int i = 1; i < num_atoms; i++) {
-  params_h[i] = {1, 2};
-}
-
-params = sycl::malloc_device<sycl::float2>(num_atoms, q_ct1);
-q_ct1.memcpy(params, params_h, num_atoms * sizeof(sycl::float2)).wait();
-
-  q_ct1.submit([&](sycl::handler &cgh) {
-    sycl::stream stream_ct1(64 * 1024, 80, cgh);
-
-    cgh.parallel_for(sycl::nd_range<3>(sycl::range<3>(1, 1, num_atoms / 200) *
+  q_ct1.parallel_for(sycl::nd_range<3>(sycl::range<3>(1, 1, num_bonds / 200) *
                                            sycl::range<3>(1, 1, 200),
                                        sycl::range<3>(1, 1, 200)),
                      [=](sycl::nd_item<3> item_ct1) {
                        computeBondedForces(forceBuffer, energyBuffer, posq,
                                            atomIndices0_0, params, num_atoms,
-                                           item_ct1, stream_ct1);
+                                           item_ct1);
                      });
-  });
+
+q_ct1.memcpy(energyBuffer_h, energyBuffer, num_bonds * sizeof(float)).wait();
+for (int i = 0; i < num_bonds; i++) {
+    //0.5*k*(sqrt(3*p**2)-r0)**2
+    //0.5*2*(sqrt(75)-1)**2
+    assert(fabs(energyBuffer_h[i] - 58.6795) <= 0.0001);
+    //printf("%6.4lf\n", energyBuffer_h[i]);
+}
 
 /*
-DPCT1003:14: Migrated API does not return error code. (*, 0) is inserted. You
+DPCT1003:12: Migrated API does not return error code. (*, 0) is inserted. You
 may need to rewrite this code.
 */
 int cudaerr = (dev_ct1.queues_wait_and_throw(), 0);
--- bond.cu
+++ bond.cu
@@ -1,7 +1,7 @@
 #include<iostream>
+#include <cassert>  
 
 #define KERNEL extern "C" __global__
-#define PADDED_NUM_ATOMS 32
 
 inline __device__ float3 make_float3(float a) {
     return make_float3(a, a, a);
@@ -30,9 +30,8 @@ __device__ inline long long realToFixedPoint(float x) {
 extern "C" __global__ void computeBondedForces(unsigned long long* __restrict__ forceBuffer,
  float* __restrict__ energyBuffer, const float4* __restrict__ posq,
   const uint2* __restrict__ atomIndices0_0, float2* params, int num_atoms) {
-
 float energy = 0;
-for (unsigned int index = blockIdx.x*blockDim.x+threadIdx.x; index < num_atoms; index += blockDim.x*gridDim.x) {
+for (unsigned int index = blockIdx.x*blockDim.x+threadIdx.x; index < num_atoms-1; index += blockDim.x*gridDim.x) {
     uint2 atoms0 = atomIndices0_0[index];
     unsigned int atom1 = atoms0.x;
     float4 pos1 = posq[atom1];
@@ -54,15 +53,16 @@ float3 force1 = delta;
 float3 force2 = -delta;
 
     atomicAdd(&forceBuffer[atom1], static_cast<unsigned long long>(realToFixedPoint(force1.x)));
-    atomicAdd(&forceBuffer[atom1+PADDED_NUM_ATOMS], static_cast<unsigned long long>(realToFixedPoint(force1.y)));
-    atomicAdd(&forceBuffer[atom1+PADDED_NUM_ATOMS*2], static_cast<unsigned long long>(realToFixedPoint(force1.z)));
+    atomicAdd(&forceBuffer[atom1+num_atoms], static_cast<unsigned long long>(realToFixedPoint(force1.y)));
+    atomicAdd(&forceBuffer[atom1+num_atoms*2], static_cast<unsigned long long>(realToFixedPoint(force1.z)));
     __threadfence_block();
     atomicAdd(&forceBuffer[atom2], static_cast<unsigned long long>(realToFixedPoint(force2.x)));
-    atomicAdd(&forceBuffer[atom2+PADDED_NUM_ATOMS], static_cast<unsigned long long>(realToFixedPoint(force2.y)));
-    atomicAdd(&forceBuffer[atom2+PADDED_NUM_ATOMS*2], static_cast<unsigned long long>(realToFixedPoint(force2.z)));
+    atomicAdd(&forceBuffer[atom2+num_atoms], static_cast<unsigned long long>(realToFixedPoint(force2.y)));
+    atomicAdd(&forceBuffer[atom2+num_atoms*2], static_cast<unsigned long long>(realToFixedPoint(force2.z)));
     __threadfence_block();
 }
 energyBuffer[blockIdx.x*blockDim.x+threadIdx.x] += energy;
+//printf("%6.4lf\n", energy);
 }
 
 
@@ -72,62 +72,78 @@ if (argc != 2) {
     printf("Usage: %s <NINT>\n", argv[0]);
     exit(1);
 }
-int num_atoms = atoi(argv[1]);
-//int num_atoms = 10000;
 
-//make idxs
-uint2* atomIndices0_0 = new uint2[num_atoms]();
-uint2* atomIndices0_0_h = new uint2[num_atoms]();
-for (unsigned int i = 0; i < num_atoms; i++) {
-  atomIndices0_0_h[i] = {0, i};
+int num_atoms = (atoi(argv[1])*200)+1;
+int num_bonds = num_atoms-1;
+
+//x, y, z for atoms which have a spring connected to 0,0,0
+float p = 5.0;
+//spring constant for all springs
+float k = 2;
+//equilibrium separation for all springs
+float r0 = 1;
+
+//make bond idxs (these ids denote what atoms have springs between them)
+uint2* atomIndices0_0 = new uint2[num_bonds]();
+uint2* atomIndices0_0_h = new uint2[num_bonds]();
+for (unsigned int i = 0; i < num_bonds; i++) {
+  atomIndices0_0_h[i] = {0, i+1};
 }
 
-cudaMalloc((void **) &atomIndices0_0, num_atoms*sizeof(uint2));
-cudaMemcpy(atomIndices0_0, atomIndices0_0_h, num_atoms*sizeof(uint2), cudaMemcpyHostToDevice);
+cudaMalloc((void **) &atomIndices0_0, num_bonds*sizeof(uint2));
+cudaMemcpy(atomIndices0_0, atomIndices0_0_h, num_bonds*sizeof(uint2), cudaMemcpyHostToDevice);
 
-//make force running total
-unsigned long long* forceBuffer = new unsigned long long[num_atoms]();
-unsigned long long* forceBuffer_h = new unsigned long long[num_atoms]();
-for (int i = 0; i < num_atoms; i++) {
-  forceBuffer_h[i] = 0.0;
+//make bond params, (r0, k)
+float2* params = new float2[num_bonds]();
+float2* params_h = new float2[num_bonds]();
+for (int i = 0; i < num_bonds; i++) {
+  params_h[i] = {r0, k};
 }
 
-cudaMalloc((void **) &forceBuffer, num_atoms*sizeof(unsigned long long));
-cudaMemcpy(forceBuffer, forceBuffer_h, num_atoms*sizeof(unsigned long long), cudaMemcpyHostToDevice);
+cudaMalloc((void **) &params, num_bonds*sizeof(float2));
+cudaMemcpy(params, params_h, num_bonds*sizeof(float2), cudaMemcpyHostToDevice);
 
 //make energy running total
-float* energyBuffer = new float[num_atoms]();
-float* energyBuffer_h = new float[num_atoms]();
-for (int i = 0; i < num_atoms; i++) {
+float* energyBuffer = new float[num_bonds]();
+float* energyBuffer_h = new float[num_bonds]();
+for (int i = 0; i < num_bonds; i++) {
   energyBuffer_h[i] = 0.0;
 }
 
-cudaMalloc((void **) &energyBuffer, num_atoms*sizeof(float));
-cudaMemcpy(energyBuffer, energyBuffer_h, num_atoms*sizeof(float), cudaMemcpyHostToDevice);
+cudaMalloc((void **) &energyBuffer, num_bonds*sizeof(float));
+cudaMemcpy(energyBuffer, energyBuffer_h, num_bonds*sizeof(float), cudaMemcpyHostToDevice);
 
-//make positions
+//make force running total
+unsigned long long* forceBuffer = new unsigned long long[num_atoms*3]();
+unsigned long long* forceBuffer_h = new unsigned long long[num_atoms*3]();
+for (int i = 0; i < num_atoms*3; i++) {
+  forceBuffer_h[i] = 0.0;
+}
+
+cudaMalloc((void **) &forceBuffer, 3*num_atoms*sizeof(unsigned long long));
+cudaMemcpy(forceBuffer, forceBuffer_h, 3*num_atoms*sizeof(unsigned long long), cudaMemcpyHostToDevice);
+
+//make positions (Place one atom at 0,0,0 and the rest at some user defined x,y,z)
 float4* posq = new float4[num_atoms]();
 float4* posq_h = new float4[num_atoms]();
 posq_h[0] = {0.0,0.0,0.0,0.0};
 for (int i = 1; i < num_atoms; i++) {
-  posq_h[i] = {5.0,5.0,5.0,0.0};
+  posq_h[i] = {p,p,p,0.0};
 }
 
 cudaMalloc((void **) &posq, num_atoms*sizeof(float4));
 cudaMemcpy(posq, posq_h, num_atoms*sizeof(float4), cudaMemcpyHostToDevice);
 
-//make bond params, (r0, k)
-float2* params = new float2[num_atoms]();
-float2* params_h = new float2[num_atoms]();
-params_h[0] = {1, 0};
-for (int i = 1; i < num_atoms; i++) {
-  params_h[i] = {1, 2};
+computeBondedForces<<<num_bonds/200, 200>>>(forceBuffer, energyBuffer, posq, atomIndices0_0, params, num_atoms);
+
+
+cudaMemcpy(energyBuffer_h, energyBuffer, num_bonds*sizeof(float), cudaMemcpyDeviceToHost);
+for (int i = 0; i < num_bonds; i++) {
+    //0.5*k*(sqrt(3*p**2)-r0)**2
+    //0.5*2*(sqrt(75)-1)**2
+    assert(abs(energyBuffer_h[i]-58.6795) <= 0.0001);
+    //printf("%6.4lf\n", energyBuffer_h[i]); 
 }
-
-cudaMalloc((void **) &params, num_atoms*sizeof(float2));
-cudaMemcpy(params, params_h, num_atoms*sizeof(float2), cudaMemcpyHostToDevice);
-
-computeBondedForces<<<num_atoms/200, 200>>>(forceBuffer, energyBuffer, posq, atomIndices0_0, params, num_atoms);
 
 cudaError_t cudaerr = cudaDeviceSynchronize();
 if (cudaerr != cudaSuccess)
